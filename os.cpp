9b44f6f70eb3a228040e13da4323b3e9abc2ce22
R Coyotl <coyotl101093@gmail.com>
2016-12-02 15:24:03 -0500

    Update os.cpp

--- os.cpp
+++ os.cpp
@@ -9,9 +9,9 @@ using namespace std;
 static bool CurrentlySwapping;
 static bool processingIO;
 static MemoryManager memory;  // creates 100k of memory
-static queue <job> IOQ; // jobs waiting for I/O
-static list <job> JobTable(); //should be intialized with space for 50 jobs
-static JobTable::iterator job; // A gloabl joberator for the job table. iterator points to the job object in the list.
+static queue <Job> IOQ; // jobs waiting for I/O
+static list <Job> JobTable; //should be intialized with space for 50 jobs
+static list<Job>::iterator job = JobTable.begin();; // A gloabl joberator for the job table. iterator points to the job object in the list.
 
 
 void startup (){
@@ -24,15 +24,15 @@ void startup (){
 
 void drmint ( int &a, int p [] ){
 	cout << "DRUM INTERRUPT" << endl << "Swap has finished. Setting flags." << endl;
-	/* Drum has finished swapping a job in/out of memory. */
+	/*  Drum has finished swapping a job in/out of memory.  */
 
-    /* move swapped job out of the job table
-     * Loop through job table looking for the job that was set to swap and set flags.
-     */
+    /*  move swapped job out of the job table
+       Loop through job table looking for the job that was set to swap and set flags
+      */
     for( job = JobTable.begin(); job != JobTable.end(); ++job ){
-           if ( *job.swapping ) {
-               *job.swapping = false;
-               *job.inMem = true;
+           if (  job->swapping ) {
+                job->swapping = false;
+                job->inMem = true;
                break; // Once job info is updated, stop iterating through list.
            }
     }
@@ -43,38 +43,38 @@ void drmint ( int &a, int p [] ){
 
 void tro ( int &a, int p [] ){
 	
-/* if  a job has reached job max allowed time, kill job
- * else if job has used up time slice but not yet used up all allowed time,
- * send back to ready queue (memory)*/
+/*  if  a job has reached job max allowed time, kill job
+ *  else if job has used up time slice but not yet used up all allowed time,
+ *  send back to ready queue (memory) */
 
-    if ( *job.curr_time >= *job.MaxTime	){    
+    if ( job->curr_time >=  job->maxTime){    
         // "kill job". Swap out not neccesary. Job table entry and memory space can be used by other job.
         cout << "TIMER RUNOUT" << endl << "TOTAL CPU TIME HAS EXCEEDED MAX TIME ALLOWED" << endl;
 		JobTable.erase( job );
     }
-    else if ( *job.curr_time <  *job.Maxtime ){
+    else if (  job->curr_time <=   job->maxTime ){
         cout << "TIMER RUNOUT" << endl << "TIME SLICE EXCEEDED, JOB SENT BACK TO MEMORY" << endl;
 
-        *job.curr_time = *job.curr_time + /* time slice  */ ;  // total curr_time + Time Slice
+         job->curr_time =  job->curr_time + /*  time slice */ ;  // total curr_time + Time Slice
     }
 }
 
 void swapper (){
-/* Determines which jobs that are not in memory to swap into memory.
- *
- * Call a routine that finds space in memory, then OS must call siodrum() to swap the job in.
- */
+/* *Determines which jobs that are not in memory to swap into memory.
+   *Call a routine that finds space in memory, then OS must call siodrum() to swap the job in.
+  */
 	if ( !CurrentlySwapping ){	
 		job = JobTable.begin();
 		while ( job != JobTable.end()){
-			if ( !(*job.inMem ) && /* job fits in memory */) {
+			if ( !( job->inMem ) && /*  job fits in memory */) {
 					CurrentlySwapping = true;
-					sos.siodrum(*job.job_num, *job.size, *job.address, 0);
+					sos.siodrum( job->job_num,  job->size,  job->address, 0);
 			}
-				else job++; // if job too big for current memory space or already in memory, move to next job in table.
+			else job++; // if job too big for current memory space or already in memory, move to next job in table.
 				
 		}
-	}
-}
 	
+	}
+	
+}
 
