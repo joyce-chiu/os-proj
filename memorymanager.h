9e3516d3e6413b33780e3993fd1614064b4f1025
tanzena92 <ctanzena@gmail.com>
2016-12-05 10:17:22 -0500

    Update MemoryManager.h file

--- memorymanager.h
+++ memorymanager.h
@@ -1,10 +1,34 @@
 #ifndef MEMORYMANAGER_H
 #define MEMORYMANAGER_H
 
+//.h file code:
+
+/// <summary>
+/// * Should be able to add to the memory
+/// delete from the memory
+/// find free space for job
+/// </summary>
+//package osProject;
+
+
+///
+/// <summary>
+/// @author Tanzena
+/// </summary>
+//TODO Tanzena
 class MemoryManager
 {
+private:
+	const int MAX_SIZE = 100;
+	std::vector<int> memory;
+
 public:
-    MemoryManager() {}
+	MemoryManager();
+
+	virtual bool addToMemory(Job *j);
+
+	virtual void removeFromMemory(Job *j);
 };
+
 
 #endif // MEMORYMANAGER_H
