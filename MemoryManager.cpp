9fb6f74768e383e0d855bd6d1ed9de9947d5c54f
tanzena92 <ctanzena@gmail.com>
2016-12-11 00:56:51 -0500

    Added FST

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -70,3 +70,62 @@ void MemoryManager::removeFromMemory(Job *j)
 		memory[i] = 0;
 	}
 }
+
+void MemoryManager::fillFreeSpaceTable()
+{
+	fsTable.clear(); // Clears the contents of the FST
+	int FSbegin = -1;
+	int FSend = -1;
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		if (memory[i] == 0)
+		{
+			FSbegin = i;
+			FSend = i;
+			for (int j = i; j < MAX_SIZE; j++)
+			{
+				if (memory[j] == 0)
+				{
+					FSend = j;
+				}
+				else if (memory[j] == 1)
+				{ //End of free space
+					break; //Break out because it found the end of free space
+				}
+			}
+			i = FSend; //To move i to the end of free space so it can look for the next one
+		}
+		if (FSbegin != -1 && FSend != -1)
+		{
+			fsTable[FSbegin] = FSend - FSbegin + 1;
+		}
+	}
+}
+
+int MemoryManager::findFreeSpace(int jobSize)
+{
+	for (auto entry : fsTable)
+	{
+		if (entry.second >= jobSize)
+		{
+			return entry.first;
+		}
+	}
+	return -1;
+}
+
+int MemoryManager::allocateMemory(int jobSize)
+{
+	// Before allocating memory we call this function determine where there is free space
+	fillFreeSpaceTable();
+	int freeSpacePos = findFreeSpace(jobSize);
+	if (freeSpacePos != -1)
+	{
+		for (int i = freeSpacePos; i < freeSpacePos + jobSize; i++)
+		{
+			memory[i] = 1;
+		}
+		return freeSpacePos;
+	}
+	return -1;
+}
