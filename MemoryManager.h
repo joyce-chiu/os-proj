d8e7a5fe5d9647ee5b8be1bf415e30c5b09f1d78
tanzena92 <ctanzena@gmail.com>
2016-12-15 00:53:11 -0500

    Update MemoryManager.h

--- MemoryManager.h
+++ MemoryManager.h
@@ -5,41 +5,14 @@ class MemoryManager
 {
 public:
 	static constexpr int MAX_SIZE = 100;
+	virtual int findFreeSpace(int jobSize); // Determines if there is enough space for the current job size
+						// If so, return the starting address else return -1 (not enough space)
+	virtual int allocateMemory(int jobSize); // Process of allocating memory for the current jobSize
+	virtual void removeFromMemory(int addressInMemory, int jobSize); // If a job is terminated then this function is called to reset the memory
+									// so it can be used by other jobs
+	MemoryManager(); // Initialize memory with 0's representing free space
 private:
 	std::vector<int> memory;
 	std::map<int, int> fsTable;
-
-	/// <summary>
-	/// memorymanager()
-	/// Initialize memory with 0's representing free space
-	/// </summary>
-public:
-	MemoryManager();
-
-	/// <summary>
-	/// fillFreeSpaceTable()
-	/// Locates where in memory there is free space and puts the free space size
-	/// as the key and the address of the free space as the value.
-	/// </summary>
-private:
-	void fillFreeSpaceTable();
-
-	/// <summary>
-	/// findFreeSpace()
-	/// Determines if there is enough space for the current job size
-	/// If so, return the starting address else return -1 (not enough space)
-	/// </summary>
-public:
-	virtual int findFreeSpace(int jobSize);
-
-	/// <summary>
-	/// Process of allocating memory for the current jobSize
-	/// </summary>
-	virtual int allocateMemory(int jobSize);
-
-	/// <summary>
-	/// If a job is terminated then this function is called to reset the memory
-	/// so it can be used by other jobs
-	/// </summary>
-	virtual void removeFromMemory(int addressInMemory, int jobSize);
-};
+	void fillFreeSpaceTable(); // Locates where in memory there is free space and puts the free space size
+				  // as the key and the address of the free space as the value.
