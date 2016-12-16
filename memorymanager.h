91bf0168288b1f58a50224e40b090d94d03ec9ab
R Coyotl <coyotl101093@gmail.com>
2016-12-16 00:34:51 -0500

    Update memorymanager.h

--- memorymanager.h
+++ memorymanager.h
@@ -16,19 +16,27 @@
 /// @author Tanzena
 /// </summary>
 //TODO Tanzena
+#ifndef MEMORYMANAGER_H
+
+#include <vector>
+#include <map>
+
 class MemoryManager
 {
-private:
-	const int MAX_SIZE = 100;
-	std::vector<int> memory;
-
 public:
-	MemoryManager();
-
-	virtual bool addToMemory(Job *j);
-
-	virtual void removeFromMemory(Job *j);
+        static constexpr int MAX_SIZE = 100;
+        virtual int findFreeSpace(int jobSize); // Determines if there is enough space for the current job size
+                                                // If so, return the starting address else return -1 (not enough space)
+        virtual int allocateMemory(int jobSize); // Process of allocating memory for the current jobSize
+        virtual void removeFromMemory(int addressInMemory, int jobSize); // If a job is terminated then this function is called to reset the memory
+                                                                        // so it can be used by other jobs
+        MemoryManager(); // Initialize memory with 0's representing free space
+private:
+        vector<int> memory;
+        map<int, int> fsTable;
+        void fillFreeSpaceTable(); // Locates where in memory there is free space and puts the free space size
+                                  // as the key and the address of the free space as the value.
 };
 
-
 #endif // MEMORYMANAGER_H
+
