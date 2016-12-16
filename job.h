021f8a2ec9f1d88a217f8a6cdf2c75b5b91758b4
R Coyotl <coyotl101093@gmail.com>
2016-12-16 00:23:12 -0500

    Update job.h

--- job.h
+++ job.h
@@ -2,45 +2,55 @@
 #define JOB_H
 
 class Job {
-public:
-    int job_num;
-    int priority;
-    int size;
-    int maxTime;
-    int curr_time;
-    int address;
-    bool inMem;
-    bool doingIO;
-    bool requestingIO;
-    bool swapping;
-    bool running;
-	
-    Job::Job() {
-        job_num = -1;
-        priority = -1;
-        size = 0 ;
-        maxTime = -1;
-        curr_time = -1;
-        address = -1;
-        inMem = false;
-        doingIO = false;
-        requestingIO = false;
-        swapping = false;
-        running = false;
-    }
-    Job::Job( int num, int priority, int size, int max_time , int current_time){
-        job_num = num;
-        priority = priority;
-        size = size ;
-        maxTime = time;
-        curr_time = current_time;
-        address = -1 ;
-        inMem = false;
-        doingIO = false;
-        requestingIO = false;
-        swapping = false;
-        running = false;
-    }
+	public:
+	int job_num;
+	int priority;
+	int size;
+	int maxTime;        //Max CPU time
+	int enterTime;      //Time when job begins using the CPU
+	int address;
+	bool inMem;
+	bool doingIO;
+	bool requestingIO;
+	bool swapping;
+	bool running;
+	bool killed;
+	bool blocked;
+	int currTime;
+
+	Job() {
+	job_num = -1;
+	priority = -1;
+	size = 0 ;
+	maxTime = -1;
+	enterTime = -1;
+	address = -1;
+	inMem = false;
+	doingIO = false;
+	requestingIO = false;
+	swapping = false;
+	running = false;
+	killed = false;
+	blocked = false;
+	int currTime = -1;
+	}
+
+	Job( int num, int priority, int size, int max_time , int current_time){
+	job_num = num;
+	priority = priority;
+	size = size ;
+	maxTime = max_time;
+	enterTime = current_time;
+	address = -1 ;
+	inMem = false;
+	doingIO = false;
+	requestingIO = false;
+	swapping = false;
+	running = false;
+	killed = false;
+	blocked = false;
+	int currTime = -1;
+	}
 };
 
 #endif // JOB_H
