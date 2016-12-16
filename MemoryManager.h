4b5ab2a4a645c72f25172c603c0e8229e8304e6e
tanzena92 <ctanzena@gmail.com>
2016-12-16 13:57:06 -0500

    Update MemoryManager.h

--- MemoryManager.h
+++ MemoryManager.h
@@ -8,8 +8,8 @@ public:
 	virtual int findFreeSpace(int jobSize); // Determines if there is enough space for the current job size
 						// If so, return the starting address else return -1 (not enough space)
 	virtual int allocateMemory(int jobSize); // Process of allocating memory for the current jobSize
-	virtual void removeFromMemory(int addressInMemory, int jobSize); // If a job is terminated then this function is called to reset the memory
-									// so it can be used by other jobs
+	virtual void removeFromMemory(int addressInMemory, int jobSize); // If a job is terminated then this function is called to reset 
+									// the memory so it can be used by other jobs
 	MemoryManager(); // Initialize memory with 0's representing free space
 private:
 	std::vector<int> memory;
