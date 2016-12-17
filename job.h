e1d1a706d95ee56e76cfd8aa4f4626705202a840
joyce-chiu <joyce.chiu.inbox@gmail.com>
2016-12-16 19:25:54 -0500

    Update job.h

--- job.h
+++ job.h
@@ -2,55 +2,53 @@
 #define JOB_H
 
 class Job {
-	public:
-	int job_num;
-	int priority;
-	int size;
-	int maxTime;        //Max CPU time
-	int enterTime;      //Time when job begins using the CPU
-	int address;
-	bool inMem;
-	bool doingIO;
-	bool requestingIO;
-	bool swapping;
-	bool running;
-	bool killed;
-	bool blocked;
-	int currTime;
+public:
+    int job_num;
+    int jobPriority;
+    int jobSize;
+    int maxTime;        //Max CPU time
+    int enterTime;      //Time when job begins using the CPU
+    int jobAddress;
+    bool inMem;
+    bool doingIO;
+    bool requestingIO;
+    bool swapping;
+    bool running;
+    bool killed;
+    bool blocked;
 
-	Job() {
-	job_num = -1;
-	priority = -1;
-	size = 0 ;
-	maxTime = -1;
-	enterTime = -1;
-	address = -1;
-	inMem = false;
-	doingIO = false;
-	requestingIO = false;
-	swapping = false;
-	running = false;
-	killed = false;
-	blocked = false;
-	int currTime = -1;
-	}
 
-	Job( int num, int priority, int size, int max_time , int current_time){
-	job_num = num;
-	priority = priority;
-	size = size ;
-	maxTime = max_time;
-	enterTime = current_time;
-	address = -1 ;
-	inMem = false;
-	doingIO = false;
-	requestingIO = false;
-	swapping = false;
-	running = false;
-	killed = false;
-	blocked = false;
-	int currTime = -1;
-	}
+    Job() {
+        job_num = -1;
+        jobPriority = -1;
+        jobSize = 0 ;
+        maxTime = -1;
+        enterTime = -1;
+        jobAddress = -1;
+        inMem = false;
+        doingIO = false;
+        requestingIO = false;
+        swapping = false;
+        running = false;
+        killed = false;
+        blocked = false;
+    }
+
+    Job( int num, int priority, int size, int max_time , int current_time){
+        job_num = num;
+        jobPriority = priority;
+        jobSize = size ;
+        maxTime = max_time;
+        enterTime = current_time;
+        jobAddress = -1 ;
+        inMem = false;
+        doingIO = false;
+        requestingIO = false;
+        swapping = false;
+        running = false;
+        killed = false;
+        blocked = false;
+    }
 };
 
 #endif // JOB_H
