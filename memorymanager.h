43fe883553e108d9c1c37ea7f510e5c7555c2872
joyce-chiu <joyce.chiu.inbox@gmail.com>
2016-12-16 19:13:40 -0500

    Update memorymanager.h

--- memorymanager.h
+++ memorymanager.h
@@ -1,10 +1,42 @@
 #ifndef MEMORYMANAGER_H
 #define MEMORYMANAGER_H
 
+#include "JOB.H"
+#include <vector>
+
+using namespace std;
+
+//.h file code:
+
+/// <summary>
+/// * Should be able to add to the memory
+/// delete from the memory
+/// find free space for job
+/// </summary>
+//package osProject;
+
+
+///
+/// <summary>
+/// @author Tanzena
+/// </summary>
+//TODO Tanzena
 class MemoryManager
 {
 public:
-    MemoryManager() {}
+
+    const int MAX_SIZE = 100;
+	vector<int> memory;
+	map<int, int> fsTable;
+
+	MemoryManager();
+
+	virtual bool addToMemory(Job *j);
+	virtual void removeFromMemory(Job *j);
+	void fillFreeSpaceTable();
+	int findFreeSpace(int jobSize);
+	int allocateMemory(int jobSize);
 };
+
 
 #endif // MEMORYMANAGER_H
