931ba703f5bab15d0e07264c6fe4c47d8b560312
tanzena92 <ctanzena@gmail.com>
2016-12-16 22:08:01 -0500

    Update MemoryManager.cpp

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -3,51 +3,85 @@
 #include <string>
 #include <vector>
 #include <list>
-#include <map>
 #include <iostream>
+#include <map>
+
+using namespace std;
 
 MemoryManager::MemoryManager()
 {
-	memory = vector<int>(Collections::Copies(MAX_SIZE, 0));
-	fsTable = map<int, int>();
+	/* Initialize memory with 0's representing free space */
 	
-	memory = list<>();
+	memory = vector<int>(Collections::nCopies(MAX_SIZE, 0));
+	fsTable = map<int, int>();
+}
+
+void MemoryManager::fillFreeSpaceTable()
+{
+	fsTable.clear(); // Clears the contents of the FST
+	int FSbegin = -1;
+	int FSend = -1;
 	for (int i = 0; i < MAX_SIZE; i++)
 	{
-		memory.push_back(0);
+		if (memory[i] == 0)
+		{
+			FSbegin = i;
+			FSend = i;
+			for (int j = i; j < MAX_SIZE; j++)
+			{
+				if (memory[j] == 0)
+				{
+					FSend = j;
+				}
+				else if (memory[j] == 1)
+				{ //End of free space
+					break; //Break out because it found the end of free space
+				}
+			}
+			i = FSend; //To move i to the end of free space so it can look for the next one
+		}
+		if (FSbegin != -1 && FSend != -1)
+		{
+			fsTable[FSbegin] = FSend - FSbegin + 1;
+		}
 	}
 }
 
-bool MemoryManager::addToMemory(Job *j)
+int MemoryManager::findFreeSpace(int jobSize)
 {
-	cout << string("addToMemory WORKING") << endl;
-
-	int sizeNeeded = j->getJobSize();
-	bool startAddSet = false;
-	int startAdd = -1;
-	int endAdd;
-	//int space = 0;
-	for (int i = 0; i < MAX_SIZE; i++)
-	{ //find spase in memory for job
-		if (memory[i] == 0 && startAddSet == false)
+	for (auto entry : fsTable)
+	{
+		if (entry.second >= jobSize)
 		{
-			startAdd = i;
-
-			startAddSet = true;
+			return entry.first;
 		}
+	}
+	return -1;
+}
 
-
-		if ((memory[i] == 1 && startAddSet) || i == 99)
+int MemoryManager::allocateMemory(int jobSize)
+{
+	// Before allocating memory we call this function determine where there is free space
+	fillFreeSpaceTable();
+	int freeSpacePos = findFreeSpace(jobSize);
+	if (freeSpacePos != -1)
+	{
+		for (int i = freeSpacePos; i < freeSpacePos + jobSize; i++)
 		{
+			memory[i] = 1;
+		}
+		return freeSpacePos;
+	}
+	return -1;
+}
 
-			endAdd = i;
-			if ((endAdd - startAdd) >= sizeNeeded)
-			{ //check if right size for our job
-				j->setAddress(startAdd); //assign starting address for tis job
-				//j.setInMemory(true);
-				break;
-			}
-			else
+void MemoryManager::removeFromMemory(int addressInMemory, int jobSize)
+{
+	for (int i = addressInMemory; i < addressInMemory + jobSize; i++)
+	{
+		memory[i] = 0;
+	}
+}
 			{
 				startAddSet = false;
 			}
